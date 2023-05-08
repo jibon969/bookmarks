b06404bb938d5ab0745d1e0bbb32dde7b9b38412
jayed969 <jayed.swe@gmail.com>
2023-05-08 21:06:11 +0600

    added media files

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -53,7 +53,8 @@ ROOT_URLCONF = 'bookmarks.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        "DIRS": [BASE_DIR / "templates"],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        # "DIRS": [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -113,10 +114,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
 
-# Add these new lines
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
