a47ca6691c941617d58f4dd888448ef2a33c40ec
jibontech <jibon.belaface@gmail.com>
2023-05-11 12:26:47 +0600

    added media file

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -18,6 +18,9 @@ from django.conf.urls.static import static
 from django.urls import path, include
 from django.contrib import admin
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
@@ -26,3 +29,5 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
