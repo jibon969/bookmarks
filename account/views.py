22a65263df0ad0cd0b4802a0e1dc8bd7fc1d1869
jayed969 <jayed.swe@gmail.com>
2023-05-13 22:53:49 +0600

    added edit view

--- account/views.py
+++ account/views.py
@@ -2,9 +2,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from .forms import LoginForm, UserRegistrationForm
-from .models import Profile
-
+from .forms import (
+    LoginForm,
+    UserRegistrationForm,
+    UserEditForm,
+    UserRegistrationForm,
+    ProfileEditForm
+)
 
 def user_login(request):
     if request.method == 'POST':
@@ -40,7 +44,22 @@ def register(request):
             new_user.set_password(user_form.cleaned_data['password'])
             # Save the User object
             new_user.save()
-            return render(request,'account/registration/register_done.html', {'new_user': new_user})
+            return render(request, 'account/registration/register_done.html', {'new_user': new_user})
     else:
         user_form = UserRegistrationForm()
-    return render(request,'account/registration/register.html',{'user_form': user_form})
+    return render(request, 'account/registration/register.html', {'user_form': user_form})
+
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(
+            instance=request.user.profile, data=request.POST, files=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+    return render(request, 'account/registration/edit.html', {'user_form': user_form, 'profile_form': profile_form})
