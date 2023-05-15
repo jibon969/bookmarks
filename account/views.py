03d7036936bdf83f1a8d21725f7c7399b5ac38f3
jayed969 <jayed.swe@gmail.com>
2023-05-15 23:38:35 +0600

    remove old code

--- account/views.py
+++ account/views.py
@@ -6,22 +6,3 @@ from django.contrib.auth.decorators import login_required
 def dashboard(request):
     context = {'section': 'dashboard'}
     return render(request, 'account/dashboard.html', context)
-
-
-# @login_required
-# def edit(request):
-#     if request.method == 'POST':
-#         user_form = UserEditForm(instance=request.user, data=request.POST)
-#         profile_form = ProfileEditForm(
-#             instance=request.user.profile, data=request.POST, files=request.FILES)
-#         if user_form.is_valid() and profile_form.is_valid():
-#             user_form.save()
-#             profile_form.save()
-#     else:
-#         user_form = UserEditForm(instance=request.user)
-#         profile_form = ProfileEditForm(instance=request.user.profile)
-#     context = {
-#         'user_form': user_form,
-#         'profile_form': profile_form
-#     }
-#     return render(request, 'account/registration/edit.html', context)
