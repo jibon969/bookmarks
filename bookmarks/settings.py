c0df6174d8eb8bf748b9a3b86aa73401abe32582
jayed969 <jayed.swe@gmail.com>
2023-05-23 20:25:46 +0600

    remove some extra files

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -1,21 +1,13 @@
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'h9(sws$-kr$@f#f-%@8xne9xhwus&h&e%ds9wn5kyfjs0mta77'
-
-# SECURITY WARNING: don't run with debug turned on in production!
+SECRET_KEY = 'Jayed-Hossain-Jibon'
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
-# Application definition
 
+# Application definition
 INSTALLED_APPS = [
     'home.apps.HomeConfig',
     'django.contrib.admin',
@@ -25,7 +17,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
-
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
