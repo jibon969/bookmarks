5d17c55e47290281fa0f49f1382d5dacfd5fc724
jayed969 <jayed.swe@gmail.com>
2023-05-24 21:22:13 +0600

    added testting database

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -54,10 +54,22 @@ WSGI_APPLICATION = 'bookmarks.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test_db',              # Database Name
+        'USER': 'postgres',             # User Name
+        'PASSWORD': 'root',             # Password for Postgresql
+        'HOST': '127.0.0.1',            # Django Server
+        'PORT': '5432',                 # default port for Postgresql
     }
 }
 
