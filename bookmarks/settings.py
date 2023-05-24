ca3637ad4e746ac7ff321f9492333387aa54b8ee
jayed969 <jayed.swe@gmail.com>
2023-05-24 20:53:13 +0600

    register app contact app

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -11,6 +11,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
     'home.apps.HomeConfig',
+    'contacts.apps.ContactsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
