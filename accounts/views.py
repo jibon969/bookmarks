8f2b8d8c1f3c953911e6e1a3c6433c73dd3e59d6
jayed969 <jayed.swe@gmail.com>
2023-05-26 21:00:26 +0600

    added dashboard view

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
 
 
 def user_login(request):
@@ -9,7 +10,7 @@ def user_login(request):
         form = LoginForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            user = authenticate(request,username=cd['username'], password=cd['password'])
+            user = authenticate(request, username=cd['username'], password=cd['password'])
             if user is not None:
                 if user.is_active:
                     login(request, user)
@@ -21,3 +22,9 @@ def user_login(request):
     else:
         form = LoginForm()
         return render(request, 'account/login.html', {'form': form})
+
+
+@login_required
+def dashboard(request):
+    return render(request, 'account/dashboard.html',
+                  {'section': 'dashboard'})
