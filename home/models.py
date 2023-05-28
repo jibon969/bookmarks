cd42557c1a11199fa8bb20644512bc4ec91a3645
jayed969 <jayed.swe@gmail.com>
2023-05-28 23:44:39 +0600

    added department models

--- home/models.py
+++ home/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
 
+class Department(models.Model):
+    name = models.CharField(max_length=120)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Student(models.Model):
     name = models.CharField(max_length=120)
     roll = models.PositiveIntegerField()
