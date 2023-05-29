a1629ae1c9072937d0f3556572b12c9f2af52168
jibontech <jibon.belaface@gmail.com>
2023-05-29 12:32:17 +0600

    added edit form

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', views.dashboard, name='dashboard'),
     path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit'),
 
     # change password urls
     path('password_change/',
